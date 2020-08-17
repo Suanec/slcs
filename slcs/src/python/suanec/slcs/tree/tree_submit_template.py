031776171cf777b5eaa2ce051e4c53128b264657
enzhao <enzhao@staff.weibo.com>
2020-08-17 21:12:28 +0800

    - [ENH] tree_submit_template.

---
+++ slcs/src/python/suanec/slcs/tree/tree_submit_template.py
@@ -0,0 +1,15 @@
+# !/usr/bin/python
+# -*- coding: UTF-8 -*-
+__author__ = 'enzhao'
+# Created by enzhao on 2020/8/17.
+
+from suanec.slcs.utils.tree_utils import *
+
+class Solution(object):
+    def self_testing(self):
+        pass
+
+if __name__ == '__main__':
+
+    s = Solution()
+    s.self_testing()
