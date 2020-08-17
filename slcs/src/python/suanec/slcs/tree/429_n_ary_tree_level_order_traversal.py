7082477d489460f3de469a96a8368623e9fff824
enzhao <enzhao@staff.weibo.com>
2020-08-17 21:12:24 +0800

    - [ENH] 429_n_ary_tree_level_order_traversal.

---
+++ slcs/src/python/suanec/slcs/tree/429_n_ary_tree_level_order_traversal.py
@@ -0,0 +1,32 @@
+# !/usr/bin/python
+# -*- coding: UTF-8 -*-
+__author__ = 'enzhao'
+# Created by enzhao on 2020/8/17. 
+
+from suanec.slcs.utils.tree_utils import *
+
+# https://leetcode.com/problems/n-ary-tree-level-order-traversal/
+
+class Solution(object):
+    def levelOrder(self, root):
+        """
+        :type root: NAryTreeNode
+        :rtype: List[List[int]]
+        """
+
+class Solution(object):
+    def preorder(self, root):
+        """
+        :type root: Node
+        :rtype: List[int]
+        """
+
+    def preorder_retrieve(self):
+        self.root
+
+    def self_testing(self):
+        '''
+        Input: root = [1,null,2,3,4,5,null,null,6,7,null,8,null,9,10,null,null,11,null,12,null,13,null,null,14]
+        Output: [1,2,3,6,7,11,14,4,8,12,5,9,13,10]
+        :return:
+        '''
