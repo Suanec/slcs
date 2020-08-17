d66c23b2b12416a1b09f7de16ad9399ee46ce9ef
enzhao <enzhao@staff.weibo.com>
2020-08-17 18:51:26 +0800

    - [ENH] 725_split_linked_list_in_parts.

---
+++ slcs/src/python/suanec/slcs/list/725_split_linked_list_in_parts.py
@@ -0,0 +1,52 @@
+# !/usr/bin/python
+# -*- coding: UTF-8 -*-
+__author__ = 'enzhao'
+# Created by enzhao on 2020/8/17.
+
+# https://leetcode-cn.com/problems/split-linked-list-in-parts/
+class Solution(object):
+    def splitListToParts(self, root, k):
+        """
+        :type root: ListNode
+        :type k: int
+        :rtype: List[ListNode]
+        """
+        if(root == None):
+            rst = [None for x in xrange(0,k)]
+            return rst
+        if(root.next == None):
+            rst = [None for x in xrange(0,k)]
+            rst[0] = root
+            return rst
+        cur_tail = root
+        count = 0
+        while(cur_tail != None):
+            cur_tail = cur_tail.next
+            count += 1
+        cur_tail = root
+        bucket_base = count / k
+        bucket_buffer = count % k
+        rst = [None for x in xrange(0,k)]
+        pre_tail = cur_tail
+        for i in xrange(0, bucket_buffer):
+            # rst[i].append(cur_tail)
+            rst[i] = cur_tail
+            tmp_count = bucket_base
+            while(tmp_count > 0):
+                cur_tail = cur_tail.next
+                tmp_count -= 1
+            pre_tail = cur_tail
+            cur_tail = cur_tail.next
+            pre_tail.next = None
+        for i in xrange(bucket_buffer,k):
+            if(cur_tail):
+                # rst[i].append(cur_tail)
+                rst[i] = cur_tail
+                tmp_count = bucket_base -1
+                while(tmp_count > 0):
+                    cur_tail = cur_tail.next
+                    tmp_count -= 1
+                pre_tail = cur_tail
+                cur_tail = cur_tail.next
+                pre_tail.next = None
+        return rst
