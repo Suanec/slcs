d86862c7e2bdacbaeecad8c658e591072c84f71e
enzhao <enzhao@staff.weibo.com>
2020-08-19 20:24:13 +0800

    - [ENH] initFullBinaryTree / middle_print / level_print.

--- slcs/src/python/suanec/slcs/utils/tree_utils.py
+++ slcs/src/python/suanec/slcs/utils/tree_utils.py
@@ -58,17 +58,60 @@ class queue(deque):
         print q.get()
         print q
 
-def initFullBinaryTree(source = []):
-    if(len(source) < 0):
+def initFullBinaryTree(source ="[5,4,1,null,1,null,4,2,null,2,null]"):
+    _source = eval(source.replace("null", "None"))
+    source_len = len(_source)
+    if(source_len < 0):
         return None
-    q = queue(maxsize=len(source))
-    node_sum = len(source) -1
-    for i in source:
-        q.put((i,0))
-    # root =
-    while(not q.empty()):
-        pass
+    root = TreeNode(_source[0])
+    if(source_len % 2 == 0):
+        _source.append(None)
+    q = queue()
+    q.push(root)
+    for i in xrange(1,source_len,2):
+        cur_left_val = None if(_source[i] == None) else TreeNode(_source[i])
+        cur_right_val = None if(_source[i + 1] == None) else TreeNode(_source[i + 1])
+        cur = q.get()
+        if(None != cur_left_val):
+            q.push(cur_left_val)
+        if(None != cur_right_val):
+            q.push(cur_right_val)
+        cur.left = cur_left_val
+        cur.right = cur_right_val
+    return root
+
+def middle_print(root):
+    def middle_order(root):
+        res = []
+        if(None == root): return res
+        res += middle_order(root.left)
+        res.append(root.val)
+        res += middle_order(root.right)
+        return res
+    middle_order_seq = middle_order(root)
+    print middle_order_seq
+
+def level_print(root):
+    def level_order(root):
+        res = []
+        if(None == root): return res
+        q = queue()
+        q.push(root)
+        while(not q.empty()):
+            cur = q.get()
+            cur_val = None if(None == cur) else cur.val
+            res.append(cur_val)
+            if(None != cur):
+                q.push(cur.left)
+                q.push(cur.right)
+        return res
+    level_order_seq = level_order(root)
+    print level_order_seq
 
 if __name__ == '__main__':
     q = queue()
     q.self_testing()
+    is_symmetric_source = initFullBinaryTree("[5,4,1,null,1,null,4,2,null,2,null]")
+    middle_print(is_symmetric_source)
+    level_print(is_symmetric_source)
+
